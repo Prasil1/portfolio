5edd8f7f0597c9657ac1abaedc0000359a10e6cd
Prasil1 <prasil.adhikari01@gmail.com>
2026-03-31 17:15:20 +0545

    added js in about page

---
+++ script.js
@@ -0,0 +1,12 @@
+ 
+
+
+const navbarToggle = document.querySelector(".navbar-toggle");
+const navbarMenue = document.querySelector(".navbar-menue");
+
+
+navbarToggle.addEventListener("click", ()=>{
+    navbarMenue.classList.toggle("active")
+    
+    navbarToggle.classList.toggle("active")
+})
