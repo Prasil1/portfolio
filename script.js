4d1035634d4b507f9ef05b9fcc7088671e2c6695
Prasil1 <prasil.adhikari01@gmail.com>
2026-04-02 21:25:20 +0545

    all pages updated :)

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 const navbarToggle = document.querySelector(".navbar-toggle");
 const navbarMenue = document.querySelector(".navbar-menue");
 
-navbarToggle.addEventListener("click", ()=>{
+navbarToggle.addEventListener("click", () => {
     navbarMenue.classList.toggle("active")
-    
+
     navbarToggle.classList.toggle("active")
 })
-    
+
+
+
+const typed1 = new Typed(".text1", { 
+    strings: ["Undergraduate IT Student", "Frontend Developer", "Web Developer"], 
+    typeSpeed: 100,
+    backSpeed: 50,
+    backDelay: 1000,
+    loop: true
+});
+
